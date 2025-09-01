c550815d4f9069f42f3fffb0b3a8b0a123cbf758
soheejjang0912 <66730047+soheejjang0912@users.noreply.github.com>
2025-09-01 14:08:02 +0900

    chore(swagger): Swagger UI에 JWT Bearer 인증 스키마 추가 - 'Authorize' 버튼 활성화 - bearerAuth 보안 스키마 정의

---
+++ src/main/java/com/gatieottae/backend/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.gatieottae.backend.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger/OpenAPI 인증 스키마 설정
+ * - "Authorize" 버튼을 띄우기 위한 JWT Bearer 스키마 등록
+ */
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
